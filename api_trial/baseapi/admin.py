78c95af5a1fd2523315a9af4ee04848714c81738
zora89 <zorawar.purohit@gmail.com>
2022-10-26 03:34:05 +0530

    Serializers Added from rest_framework.serializers

--- api_trial/baseapi/admin.py
+++ api_trial/baseapi/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Advocates
+
+
 
 # Register your models here.
+admin.site.register(Advocates)
--- api_trial/baseapi/views.py
+++ api_trial/baseapi/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Advocates
+from .serializers import AdvocateSerializer
 
 # Create your views here.
 @api_view(['GET'])
@@ -11,8 +13,10 @@ def endpoints(request):
 
 @api_view(['GET'])
 def advocates(request):
-    data = ['Zora', 'Time', 'Flies']
-    return Response(data)
+    #data = ['Zora', 'Time', 'Flies']
+    advocates = Advocates.objects.all()
+    serializer = AdvocateSerializer(advocates, many=True)
+    return Response(serializer.data)
 
 @api_view(['GET', 'POST'])
 def advocate_details(request, username):
